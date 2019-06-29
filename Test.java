8701bc3baa3f7b073592dc8123f400ecef583ba5
AanandBhandari <aanandbhandari143@gmail.com>
2019-06-29 23:19:13 +0545

    2k75 swing soln

--- Test.java
+++ Test.java
@@ -1,68 +1,90 @@
+
 import javax.swing.*;
 import java.awt.event.*;
-import java.awt.geom.*;
-public class Test extends JFrame{
-    JLabel enterP,enterT,enterR,intrest;
-    JTextField p,t,r;
-    JButton i;
-    public Test(){};
-    public Test(String s){
+public class Test extends JFrame {
+    JLabel heading,result;
+    JTextField str;
+    JButton palindrome,reverse,vowel;
+    Test(){}
+    Test(String s){
         super(s);
     }
-    public void setComponents() {
-        setLayout(null);
-        // intrest 
-        intrest = new JLabel();
-        intrest.setBounds(100, 170, 200, 20);
-        add(intrest);
-        // principle
-        enterP = new JLabel();
-        enterP.setText("Enter Principle");
-        enterP.setBounds(50, 50, 200, 20);
-        add(enterP);
-        p = new JTextField();
-        p.setBounds(250, 50, 80, 20);
-        add(p);
-        // time
-        enterT = new JLabel();
-        enterT.setText("Enter Time");
-        enterT.setBounds(50, 80, 200, 20);
-        add(enterT);
-        t = new JTextField();
-        t.setBounds(250, 80, 80, 20);
-        add(t);
-        // rate
-        enterR = new JLabel();
-        enterR.setText("Enter Rate");
-        enterR.setBounds(50, 110, 200, 20);
-        add(enterR);
-        r = new JTextField();
-        r.setBounds(250, 110, 80, 20);
-        add(r);
-        // caluclate Intrest button
-        i = new JButton("Calculate Intrest");
-        i.setBounds(100, 140, 200, 20);
-        i.addActionListener(new Handler());
-        add(i);
-    }
-    class Handler implements ActionListener {
-        public void actionPerformed(ActionEvent e){
-            int pr = Integer.parseInt(p.getText());
-            int ti = Integer.parseInt(t.getText());
-            int ra = Integer.parseInt(r.getText());
-            double in = (pr*ti*ra)/100;
-            // System.out.println(in);
-            intrest.setText("Intrest = "+in);
-        }
-    }
     public static void main(String[] args) {
-        Test jf = new Test("simple interest");
+        Test jf = new Test("Helloworld");
         jf.setComponents();
         jf.setVisible(true);
-        // jf.setShape(new Ellipse2D.Float(20f, -30f, 250.0f, 150.0f));
         jf.setSize(500,500);
-        jf.setLocation(500,300);
         jf.setDefaultCloseOperation(jf.EXIT_ON_CLOSE);
-
+    }
+    public void setComponents() {
+        // heading
+        setLayout(null);
+        heading = new JLabel("Input any String");
+        heading.setBounds(50,50,200,20);
+        add(heading);
+        // Input field
+        str = new JTextField();
+        str.setBounds(50,80,100,20);
+        add(str);
+        // palindrome button
+        palindrome = new JButton("Find Palindrome");
+        palindrome.setBounds(50, 110, 200, 20);
+        add(palindrome);
+        palindrome.addActionListener(new PalindromeHandler());
+        // reverse button
+        reverse = new JButton("Find Reverse");
+        reverse.setBounds(50, 140, 200, 20);
+        add(reverse);
+        reverse.addActionListener(new ReverseHandler());
+        // vowel button
+        vowel = new JButton("Find Vowel");
+        vowel.setBounds(50, 170, 200, 20);
+        add(vowel);
+        vowel.addActionListener(new VowelHandler());
+        // result
+        result = new JLabel();
+        result.setBounds(50,200,200,20);
+        add(result);
+    }
+    class PalindromeHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e){
+            String s = str.getText();
+            int length = s.length();
+            String rev = "";
+            for (int i = length - 1; i >= 0; i--) {
+                rev = rev + s.charAt(i);
+            }
+            if (s.equals(rev)) {
+                result.setText(s + " is a Palindrome");
+            } else {
+                result.setText(s + " is not Palindrome");
+            }
+        }
+    }
+    
+    class ReverseHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String s = str.getText();
+            int length = s.length();
+            String rev = "";
+            for (int i = length - 1; i >= 0; i--) {
+                rev = rev + s.charAt(i);
+            }
+            result.setText(rev + " is a reverse of "+s);
+        }
+    }
+    class VowelHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String s = str.getText();
+            s.toLowerCase();
+            int length = s.length();
+            String vowel = "";
+            for (int i = length - 1; i >= 0; i--) {
+                if (s.charAt(i)=='a') {
+                    vowel = vowel+"a";
+                }
+            }
+            result.setText(vowel);
+        }
     }
 }
