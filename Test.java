55061f7df47181cacabf00607c09bf666f118cac
AanandBhandari <aanandbhandari143@gmail.com>
2019-06-29 11:06:23 +0545

    basic layout of adding two numbers

--- Test.java
+++ Test.java
@@ -1,93 +1,48 @@
-// class Box {
-//     private int l,b,h;
-//     public void setDimensions(int l,int b,int h){
-//         this.l=l;
-//         this.b=b;
-//         this.h=h;
-//     }
-//     public void getDimension(){
-//         System.out.print(l);        
-//         System.out.print(b);
-//         System.out.print(h);
-//     }
-// }
-// public class Test {
-//     public static void main(String[] args) {
-//         Box cube = new Box();
-//         cube.setDimensions(2, 3, 4);
-//         cube.getDimension();
-//     }
-// }
-
-// wrapper Class example
-// public class Test {
-//     public static void main(String[] args) {
-//         Integer i1 = Integer.valueOf("11",2);
-//         int a = Integer.parseInt("aanand");
-//         System.out.println(a);
-//         System.out.println(i1.intValue());
-//     }
-// }
-// package test;
-// public class Test {
-//     public static void main(String[] args) {
-//         System.out.println("helloworld");
-//     }
-// }
-
-// inheritance
-// public class Test {
-//     public static void main(String[] args) {
-//         Student S1 = new Student();
-//         S1.setRoll(2);
-//         S1.setAge(10);
-//         S1.setName("Aanand");
-//         System.out.println(S1.getAge());
-//         System.out.println(S1.getName());
-//         System.out.println(S1.getRoll());
-
-//     }
-// }
-
-// constructor inheritance
-// class A {
-//     public A () {
-//         System.out.println("hello from the A");
-//     }
-    
-// }
-// class B extends A{
-//     public B() {
-//         System.out.println("hello from the B");
-//     }
-// }
-// class Test {
-//     public static void main(String[] args) {
-//         new B();
-//     }
-// }
-// swing example
-// import javax.swing.*;
-// public class Test {
-//     public static void main(String[] args) {
-//         JFrame jf = new JFrame("Swing Example");
-//         jf.setVisible(true);
-//         jf.setSize(300,300);
-//         jf.setDefaultCloseOperation(jf.EXIT_ON_CLOSE);
-//     }
-// }
-// another way
 import javax.swing.*;
 
 public class Test extends JFrame {
-    public Test(){}
-    public Test(String s){
+    JLabel l1,l2,l3,l4;
+    JTextField t1,t2;
+    JButton b1;
+    public Test() {
+    }
+
+    public Test(String s) {
         super(s);
     }
+    public void setComponents() {
+        l1 = new JLabel("Addition of two number");
+        l2 = new JLabel("Enter first number");
+        l3 = new JLabel("Enter second number");
+        l4 = new JLabel();
+        t1 = new JTextField();
+        t2 = new JTextField();
+        b1 = new JButton("Add");
+
+        // setBounds to all objs
+        setLayout(null);
+        l1.setBounds(150,50,200,20);
+        l2.setBounds(50,80,200,20);
+        t1.setBounds(260,80,80,20);
+        l3.setBounds(50,110,200,20);
+        t2.setBounds(260, 110, 80, 20);
+        b1.setBounds(150, 140, 80, 20);
+        l4.setBounds(50,170,200,40);
+        // adding these objs in JFrame
+        add(l1);
+        add(l2);
+        add(t1);
+        add(l3);
+        add(t2);
+        add(b1);
+        add(l4);
+    }
+
     public static void main(String[] args) {
         Test jf = new Test("Swing Example");
+        jf.setComponents();
         jf.setVisible(true);
-        jf.setSize(300, 300);
+        jf.setSize(500, 500);
         jf.setDefaultCloseOperation(jf.EXIT_ON_CLOSE);
     }
 }
