68f2bcfc1bae32a37f374eade3893fec9b0017b8
AanandBhandari <aanandbhandari143@gmail.com>
2019-07-01 14:59:04 +0545

    multithreading example

--- Test.java
+++ Test.java
@@ -1,90 +1,23 @@
-
-import javax.swing.*;
-import java.awt.event.*;
-public class Test extends JFrame {
-    JLabel heading,result;
-    JTextField str;
-    JButton palindrome,reverse,vowel;
-    Test(){}
-    Test(String s){
-        super(s);
+class A implements Runnable {
+    public void run() {
+        for(int i=0;i<=10;i++) {
+            System.out.println("thread A "+i);
+        }
     }
+}
+class B implements Runnable {
+    public void run() {
+        for(int i = 0 ;i<=10;i++) {
+            System.out.println("thread B" + i);
+        }
+    }
+}
+public class Test {
     public static void main(String[] args) {
-        Test jf = new Test("Helloworld");
-        jf.setComponents();
-        jf.setVisible(true);
-        jf.setSize(500,500);
-        jf.setDefaultCloseOperation(jf.EXIT_ON_CLOSE);
+        Thread a = new Thread(new A());
+        Thread b = new Thread(new B());
+        a.start();
+        b.start();
     }
-    public void setComponents() {
-        // heading
-        setLayout(null);
-        heading = new JLabel("Input any String");
-        heading.setBounds(50,50,200,20);
-        add(heading);
-        // Input field
-        str = new JTextField();
-        str.setBounds(50,80,100,20);
-        add(str);
-        // palindrome button
-        palindrome = new JButton("Find Palindrome");
-        palindrome.setBounds(50, 110, 200, 20);
-        add(palindrome);
-        palindrome.addActionListener(new PalindromeHandler());
-        // reverse button
-        reverse = new JButton("Find Reverse");
-        reverse.setBounds(50, 140, 200, 20);
-        add(reverse);
-        reverse.addActionListener(new ReverseHandler());
-        // vowel button
-        vowel = new JButton("Find Vowel");
-        vowel.setBounds(50, 170, 200, 20);
-        add(vowel);
-        vowel.addActionListener(new VowelHandler());
-        // result
-        result = new JLabel();
-        result.setBounds(50,200,200,20);
-        add(result);
-    }
-    class PalindromeHandler implements ActionListener {
-        public void actionPerformed(ActionEvent e){
-            String s = str.getText();
-            int length = s.length();
-            String rev = "";
-            for (int i = length - 1; i >= 0; i--) {
-                rev = rev + s.charAt(i);
-            }
-            if (s.equals(rev)) {
-                result.setText(s + " is a Palindrome");
-            } else {
-                result.setText(s + " is not Palindrome");
-            }
-        }
-    }
-    
-    class ReverseHandler implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            String s = str.getText();
-            int length = s.length();
-            String rev = "";
-            for (int i = length - 1; i >= 0; i--) {
-                rev = rev + s.charAt(i);
-            }
-            result.setText(rev + " is a reverse of "+s);
-        }
-    }
-    class VowelHandler implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            String s = str.getText();
-            s.toLowerCase();
-            int length = s.length();
-            String vowel = "";
-            for (int i = length - 1; i >= 0; i--) {
-                if (s.charAt(i)=='a') {
-                    vowel = vowel+"a";
-                }
-            }
-            result.setText(vowel);
-        }
-    }
+
 }
